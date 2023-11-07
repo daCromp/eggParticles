e365b690b0211e68c1bedf65698a59b2ab40e517
David Crompton <davidcrompton00@gmail.com>
2023-11-07 17:05:26 +0100

    donso

--- egg_framework/scenes/particles/app.js
+++ egg_framework/scenes/particles/app.js
@@ -1,35 +1,37 @@
-import {Scene2D} from "../../core/scene.js";
-import {ParticleSystem2D} from "./particle_system_2d.js";
+import { Scene2D } from "../../core/scene.js";
+import { ParticleSystem2D } from "./particle_system_2d.js";
 
 class ParticleScene extends Scene2D {
+  constructor() {
+    super();
+    this.particleSystem = undefined;
+  }
 
-    constructor() {
-        super();
-        this.particleSystem = undefined;
+  load(appRuntime) {
+    super.load(appRuntime);
+    // create particle system
+    this.particleSystem = new ParticleSystem2D({
+      position: [350, 350],
+      velocityRangeX: [-1, 1],
+      velocityRangeY: [-1, 1],
+      size: 5,
+    });
+    // this.particleSystem.createParticles();
+  }
+
+  update(deltaTime) {
+    super.update(deltaTime);
+    if (this.particleSystem === undefined) return;
+    this.particleSystem.update(deltaTime);
+    if (this.particleSystem._particles.length < 10000) {
+      this.particleSystem.createParticles();
     }
+  }
 
-
-    load(appRuntime) {
-        super.load(appRuntime);
-
-        // create particle system
-        this.particleSystem = new ParticleSystem2D({});
-        // this.particleSystem.createParticles();
-    }
-
-    update(deltaTime) {
-        super.update(deltaTime);
-        if(this.particleSystem === undefined) return;
-        this.particleSystem.update(deltaTime);
-        if(this.particleSystem._particles.length < 10000) {
-            this.particleSystem.createParticles();
-        }
-    }
-
-    render() {
-        super.render();
-        this.particleSystem.render(this.ctx);
-    }
+  render() {
+    super.render();
+    this.particleSystem.render(this.ctx);
+  }
 }
 
 export default ParticleScene;
--- egg_framework/scenes/particles/particle_system_2d.js
+++ egg_framework/scenes/particles/particle_system_2d.js
@@ -1,44 +1,49 @@
-import {rand} from "../../utils.js";
-import {Particle} from "./particle.js";
+import { rand } from "../../utils.js";
+import { Particle } from "./particle.js";
 
 class ParticleSystem2D {
+  /*
+   * A particle system does the book keeping of particles.
+   */
+  constructor(config) {
+    this._particles = [];
+    this._position = config.position;
+    this._size = config.size;
+    this._velocityRangeX = config.velocityRangeX;
+    this._velocityRangeY = config.velocityRangeY;
+  }
 
-    /*
-    * A particle system does the book keeping of particles.
-    */
-    constructor(config) {
-        this._particles = [];
+  createParticles() {
+    // TODO: implement better creation
+    for (let i = 0; i < 1; ++i) {
+      let particle = new Particle({
+        position: [this._position[0], this._position[1]],
+        velocity: [
+          rand.rand(this._velocityRangeX[0], this._velocityRangeX[1]),
+          rand.rand(this._velocityRangeY[0], this._velocityRangeY[1]),
+        ],
+        size: this._size,
+      });
+      this._particles.push(particle);
     }
+  }
 
-    createParticles() {
-        // TODO: implement better creation
-        for (let i=0; i<1; ++i) {
-            let particle = new Particle({
-                position : [350, 350],
-                velocity : [rand.rand(-1, 1), rand.rand(-1, 1)],
-                size     : 5
-            })
-            this._particles.push(particle)
-        }
+  render(context) {
+    for (let particle of this._particles) {
+      particle.render(context);
     }
+  }
 
-    render(context) {
-        for (let particle of this._particles) {
-            particle.render(context)
-        }
+  update(deltaTime) {
+    // update the particles
+    for (let particle of this._particles) {
+      particle.update();
+      if (particle._lifetime <= 0) {
+        this._particles.shift();
+      }
     }
-
-    update(deltaTime) {
-        // update the particles
-        for (let particle of this._particles) {
-            particle.update()
-            if(particle._lifetime <= 0){
-                this._particles.shift();
-            }
-        }
-        // TODO: more logic over the particles if necessary
-    }
-
+    // TODO: more logic over the particles if necessary
+  }
 }
 
-export {ParticleSystem2D};
+export { ParticleSystem2D };
